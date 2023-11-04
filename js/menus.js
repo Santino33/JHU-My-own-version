c514af35022f8ec86dca8ee8db67d426af15ef9d
Santino33 <davidnaranjo330@gmail.com>
2023-11-04 18:55:59 -0500

    Main site - header menus completed

--- js/menus.js
+++ js/menus.js
@@ -2,9 +2,19 @@ const hamburguer = document.getElementById("header-menu");
 const search = document.getElementById("header-search");
 const menu = document.querySelector(".menu-section1");
 const menuSearch = document.querySelector(".menu-section2");
+const input = document.querySelector(".input");
+const inputPressed = document.querySelector(".input-pressed")
 
 hamburguer.addEventListener('click', () =>{
     menu.classList.toggle('spread')
+})
+
+search.addEventListener('click', () =>{
+    menuSearch.classList.toggle('spread')
+})
+
+input.addEventListener('click', () =>{
+    input.classList.toggle('inputPressed')
 })
 
 window.addEventListener('click', e=>{
@@ -12,4 +22,15 @@ window.addEventListener('click', e=>{
         menu.classList.toggle('spread');
     }
        
+})
+
+window.addEventListener('click', e=>{
+    if(menuSearch.classList.contains('spread') && e.target!= menuSearch && e.target != search && e.target != input){
+        menuSearch.classList.toggle('spread');
+    }
+       
+})
+
+window.addEventListener('click', e=>{
+    
 })
